d5f8b2ebf2b6dbc79c34892591c554a7387e28f1
ErnieBenner <40174361+ErnieBenner@users.noreply.github.com>
2018-09-25 22:40:32 -0400

    Update calls.py

--- calls.py
+++ calls.py
@@ -1,48 +1,56 @@
 # -*- coding: utf-8 -*-
 import requests, json, os
 
-
-base_url = 'https://api.groupme.com/v3/'
-token = os.environ['GM_API_KEY']
-bot_id = os.environ['ROOMMATE_BOT_ID']
+#This change might allow for there to be one class with general functions, which can then be subclassed to allow specific api calls. 
+# a sub class for each messaging platform. 
+#base_url = 'https://api.groupme.com/v3/'
+#token = os.environ['GM_API_KEY']
+#bot_id = os.environ['ROOMMATE_BOT_ID']
 
 class NotReached(Exception):
     pass
 
-
-def post_message(string, url="https://api.groupme.com/v3/bots/post", bot_id=bot_id):
-    """Posts a string to the groupchat arguments"""
-
-    data = {
-    "text": string,
-    "bot_id": bot_id
-    }
-
-    r = requests.post(url, data=data)
-
-def read_messages(n, url=(base_url + "groups/38611088/messages?")):
-    """Returns a list of the last n messages details as (string, sender, time)
-    if request fails raise NotReached"""
-
-    limit = "&limit=%d&" % n
-    url = url + limit + token
-    r = requests.get(url)
+class Caller(object):
     
-    try:
+    
+    def __init__(self, base_url, token, bot_id):
+        self.base_url = base_url
+        self.token = token
+        self.bot_id = bot_id
+    
+    def post_message(self, string, url=(self.base_url + "/bots/post"):
+        """Posts a string to the groupchat arguments"""
 
-        messages = json.loads(r.text)['response']["messages"]
-        return [(messes['text'], messes['name'], messes['created_at'], messes['id']) for messes in messages]
+        data = {
+        "text": string,
+        "bot_id": self.bot_id
+        }
 
-    except TypeError:
+        r = requests.post(url, data=data)
 
-        raise NotReached("read_message request call returned 'None'")
+    def read_messages(self, n, url=(self.base_url + "groups/38611088/messages?")):
+        """Returns a list of the last n messages details as (string, sender, time, message id)
+        if request fails raise NotReached"""
 
-    except KeyError:
+        limit = "&limit=%d&" % n
+        url = url + limit + self.token
+        r = requests.get(url)
 
-        error_string = "Either a groupme update broke your system, or response code isn't 200. Response code: 
-        %d" % json.loads(r.text)['meta']['code']
-        raise NotReached(error_string)
+        try:
 
-def like(id, url=""):
-    """Likes message by message id **unimplimented**"""
-    pass
+            messages = json.loads(r.text)['response']["messages"]
+            return [(messes['text'], messes['name'], messes['created_at'], messes['id']) for messes in messages]
+
+        except TypeError:
+
+            raise NotReached("read_message request call returned 'None'")
+
+        except KeyError:
+
+            error_string = "Either a groupme update broke your system, or response code isn't 200. Response code: 
+            %d" % json.loads(r.text)['meta']['code']
+            raise NotReached(error_string)
+
+    def like(id, url=""):
+        """Likes message by message id **unimplimented**"""
+        pass
